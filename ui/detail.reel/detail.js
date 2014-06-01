c05d975e7041b80a78f37144e92e540abfb848cf
lothaeron <noreply>
2014-06-01 02:00:07 +0000

    Add component detail

---
+++ ui/detail.reel/detail.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/detail.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Detail
+ * @extends Component
+ */
+exports.Detail = Component.specialize(/** @lends Detail# */ {
+    constructor: {
+        value: function Detail() {
+            this.super();
+        }
+    }
+});
